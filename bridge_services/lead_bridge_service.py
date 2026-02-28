72a8169c95980d26d92f6b7634372e15a86825b0
Snap <mshahid00989800@gmail.com>
2026-02-28 15:15:46 +0530

    building bridge

--- bridge_services/lead_bridge_service.py
+++ bridge_services/lead_bridge_service.py
@@ -1,6 +1,36 @@
+import asyncio
+from gmqtt import Client as mqtt
+import uuid
+import ssl
+from functools import partial
+import json
+import os
+import dotenv
+current_status="RUNNING"
+current_condition="NORMAL"
+service_id=str(uuid.uuid4())
+print(service_id)
+subscribe_topics={
+                "bridge_call_topic":"00989800/to_bridge_calls" 
+                }
 
-def update_health_check():
-    pass
+publish_topics={
+                "bridge_call_recv_topic":"00989800/from_bridge_calls"
+            }
+# _______ callbacks ______________
+
+def on_connect(client,flags, rc, properties):
+    global subscribe_topics
+    
+    client.subscribe(subscribe_topics['bridge_call_topic'])
+    print(f"subscribed to topic:{subscribe_topics['bridge_call_topic']}")
+    print()
+
+
+
+def update_health_check(publish_topics):
+    global service_id,current_status
+    data={"service_id":service_id,"status":current_status,"condition":current_condition}
 
 def get_data():
     pass
@@ -23,3 +53,24 @@ def start_service():
 def go_idle():
     pass
 
+async def main():
+    
+    
+    host="31d09ce8b7fa4a92aafc62ae06187541.s1.eu.hivemq.cloud"
+    port=8883
+    username="Snappp"
+    password="Snap00989800"
+    ssl_ctx = ssl.create_default_context()
+
+    client=mqtt(service_id)
+    client.set_auth_credentials(username=username, password=password)
+
+    client.on_connect=on_connect
+    client.on_message = partial(on_message,incomming_calls_queue=incomming_calls_queue)
+    client.on_disconnect = on_disconnect
+    client.on_subscribe=on_subscribe
+
+    await client.connect(host=host, port=port, ssl=ssl_ctx)
+
+if __name__=='__main__':
+    asyncio.run(main())
