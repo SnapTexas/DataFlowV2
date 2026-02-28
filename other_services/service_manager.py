b7818811939257984a5ad44e910588b2bf3f33ed
Snap <mshahid00989800@gmail.com>
2026-02-28 14:44:12 +0530

    creating validation service talkinh to long..

--- other_services/service_manager.py
+++ other_services/service_manager.py
@@ -18,18 +18,31 @@ collect_bridge_status_reponse=False
 active_bridge_service_set = set()
 #needs partial
 
-def on_connect(client, flags, rc, properties):
+def on_connect(bridge_client, flags, rc, properties):
     global subscribe_topics
-    for i,j in subscribe_topics.items():
-        client.subscribe(j)
-        print(f"subscribed to {i}")
+    bridge_client.subscribe(subscribe_topics['bridge_call_recv_topic'])
+    #for i,j in subscribe_topics.items():
+    #    bridge_client.subscribe(j)
+    #    print(f"subscribed to {i}")
 
-async def on_message(client, topic, payload, qos, properties,incomming_calls_queue):# when we get a msg put it in queue for worker to process
+def on_connect_validation(validation_client, flags, rc, properties):
+    global subscribe_topics
+    validation_client.subscribe(subscribe_topics['validation_call_recv_topic'])
+    
+async def on_message(bridge_client, topic, payload, qos, properties,incomming_calls_queue):# when we get a msg put it in queue for worker to process
     #put the data in the calls queue for worker to process
     service_data={"topic":topic,
           "data":payload.decode('utf-8')}
     service_data=json.dumps(service_data)
     await incomming_calls_queue.put(service_data)
+
+async def on_message_validation(validation_client, topic, payload, qos, properties,validation_incomming_calls_queue):
+    #put the data in the calls queue for worker to process
+    service_data={"topic":topic,
+          "data":payload.decode('utf-8')}
+    service_data=json.dumps(service_data)
+    await validation_incomming_calls_queue.put(service_data)
+
 
 def get_active_bridge_services(service_data):# gets or updates the result active bridge services set 
     global active_bridge_service_set
@@ -41,11 +54,21 @@ def get_active_bridge_services(service_data):# gets or updates the result active
         #Removing idle services from set of running services 
         active_bridge_service_set.remove(service_data['service_id'])
 
-async def get_bridge_services_status(client,call_to_service_topic):# send a msg to all bridge services to respond with status which is collected by worker
+def get_active_validation_services(service_data):# gets or updates the result active bridge services set 
+    global active_validation_service_set
+    service_data=json.loads(service_data)
+    if service_data['status']=="RUNNING" :
+        #Adding to set of running services 
+        active_bridge_service_set.add(service_data['service_id'])
+    elif service_data['status']=="IDLE" and service_data['service_id'] in active_bridge_service_set:
+        #Removing idle services from set of running services 
+        active_bridge_service_set.remove(service_data['service_id'])
+
+async def get_bridge_services_status(bridge_client,call_to_service_topic):# send a msg to all bridge services to respond with status which is collected by worker
     global active_bridge_service_set,thresh_bridge_services,bridge_status_response_calls,collect_bridge_status_reponse
     service_data={"service_id":"ALL","msg":"STATUS","condition":None}
     service_data=json.dumps(service_data)
-    client.publish(call_to_service_topic,service_data)
+    bridge_client.publish(call_to_service_topic,service_data)
     #For now simply sleep for 10s and then collect whatever the response was 
     flush_bridge_status_list()
     collect_bridge_status_reponse=True
@@ -82,26 +105,26 @@ def choose_service_stop(list_of_services):# takes a list of services and returns
     else:
         return None 
 
-async def start_bridge_service(client,service_id): # starts the service whose id is given (Commands to start)
+async def start_bridge_service(bridge_client,service_id): # starts the service whose id is given (Commands to start)
     #Command to start processing messages
     start_service_topic=f"00989800/call_to_bridge_service/{service_id}"
-    client.publish(start_service_topic,"START")
+    bridge_client.publish(start_service_topic,"START")
     
-async def stop_bridge_service(client,service_id):# stops the service whose id is given (Commands to start)
+async def stop_bridge_service(bridge_client,service_id):# stops the service whose id is given (Commands to start)
     #Stop means to go idle
     start_service_topic=f"00989800/call_to_bridge_service"
     service_data={"service_id":service_id,
           "msg":"IDLE",
           "condition":None}
     service_data=json.dumps(service_data)
-    client.publish(start_service_topic,service_data)
+    bridge_client.publish(start_service_topic,service_data)
 
 async def verify_bridge_service_call_response(calls_to_bridge_services):# will verify the result of calls made to bridge services 
     pass
     # look at the queue who were messaged did they respond successfully
     # if yess inform if no inform or ping them to see if they are alive else till dead service
 
-async def respond_msg(client,topic:str,msg:str):
+async def respond_msg(bridge_client,topic:str,msg:str):
     global active_bridge_service_set,bridge_status_response_calls
     # 1. Parse once at the top
     try:
@@ -114,24 +137,24 @@ async def respond_msg(client,topic:str,msg:str):
         #Bridge service response
         service_data=json.loads(msg)
         if service_data['condition']=="UNDERLOAD":
-            await get_bridge_services_status(client=client,call_to_service_topic=publish_topics['bridge_call_topic'])
+            await get_bridge_services_status(bridge_client=bridge_client,call_to_service_topic=publish_topics['bridge_call_topic'])
             chosen_service_id=choose_service_start(bridge_status_response_calls)
-            await start_bridge_service(client,chosen_service_id)  
+            await start_bridge_service(bridge_client,chosen_service_id)  
         elif service_data['condition']=="NORMAL" and service_data['status']!="IDLE":# check if the running services are normal
             #Command One of the service to shut down if there are more than one service running on that topic
             #get list of active services on that topic if there are more than one services active on that topic stop one
-            await get_bridge_services_status(client=client,call_to_bridge_service_topic=publish_topics['bridge_call_topic'])
+            await get_bridge_services_status(bridge_client=bridge_client,call_to_bridge_service_topic=publish_topics['bridge_call_topic'])
             chosen_service_id=choose_service_stop(bridge_status_response_calls)
-            await stop_bridge_service(client=client,service_id=chosen_service_id)
+            await stop_bridge_service(bridge_client=bridge_client,service_id=chosen_service_id)
             
     elif topic==subscribe_topics['validation_call_recv_topic']:
         #Validations service response
         pass
     else:
         pass
-    #client.publish(topic,msg)
+    #bridge_client.publish(topic,msg)
 
-async def worker(client,queue):# checks the queue for incomming calls and sends them to be processed and maintains the set of active bridge services
+async def worker(bridge_client,queue):# checks the queue for incomming calls and sends them to be processed and maintains the set of active bridge services
     while True:
         
         msg = await queue.get()   # waits without blocking
@@ -145,14 +168,14 @@ async def worker(client,queue):# checks the queue for incomming calls and sends 
                 collect_bridge_service_status_response(decoded_data)
             except:
                 pass
-        await respond_msg(client,msg['topic'],msg['data'])        # slow work here
+        await respond_msg(bridge_client,msg['topic'],msg['data'])        # slow work here
         queue.task_done()
 
-def on_disconnect(client, packet, exc=None):# just prints diconnected used to see when disconnected
+def on_disconnect(bridge_client, packet, exc=None):# just prints diconnected used to see when disconnected
     print("Disconnected ...")
     print()
 
-def on_subscribe(client, mid, qos, properties):# just prints subscribed used to see when disconnected
+def on_subscribe(bridge_client, mid, qos, properties):# just prints subscribed used to see when disconnected
     print("subscribed to topics")
     print()
 
@@ -164,21 +187,39 @@ async def main():
     port=8883
     username="Snappp"
     password="Snap00989800"
-    client_id=str(uuid.uuid4())
-    client=mqtt(client_id)
+    bridge_client_id=str(uuid.uuid4())
+    
+    bridge_client=mqtt(bridge_client_id)
+    
+    
 
-    client.set_auth_credentials(username=username, password=password)
+    bridge_client.set_auth_credentials(username=username, password=password)
+
+    
 
     ssl_ctx = ssl.create_default_context()
-    client.on_connect=on_connect
-    client.on_message = partial(on_message,incomming_calls_queue=incomming_calls_queue)
-    client.on_disconnect = on_disconnect
-    client.on_subscribe=on_subscribe
+    bridge_client.on_connect=on_connect
+    bridge_client.on_message = partial(on_message,incomming_calls_queue=incomming_calls_queue)
+    bridge_client.on_disconnect = on_disconnect
+    bridge_client.on_subscribe=on_subscribe
 
-    await client.connect(host=host,
+    await bridge_client.connect(host=host,
                          port=port,
                          ssl=ssl_ctx)
-    asyncio.create_task(worker(client=client,queue=incomming_calls_queue))
+    
+
+
+    ####Client 2
+    validation_client_id=str(uuid.uuid4())
+    validation_client=mqtt(validation_client_id)
+    validation_client.set_auth_credentials(username=username, password=password)
+    validation_client.on_connect= on_connect_validation
+    validation_client.on_message = partial(on_message_validation,incomming_calls_queue=incomming_calls_queue)
+    validation_client.on_disconnect = on_disconnect_validation
+    validation_client.on_subscribe=on_subscribe_validation
+
+
+    asyncio.create_task(worker(bridge_client=bridge_client,queue=incomming_calls_queue))
 
     await asyncio.Event().wait()
 
